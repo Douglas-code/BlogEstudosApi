a30e1f97913b1cae1bba0b05db86f8fcc85f33ef
Douglas Rodrigues <douglasrodrigues20161@outlook.com>
2020-11-29 15:38:40 -0300

    Adicionando segunda migration e atualizando base de dados

--- BlogEstudos/Migrations/20201129011958_SecondMigrations.Designer.cs
+++ BlogEstudos/Migrations/20201129011958_SecondMigrations.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using BlogEstudos.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace BlogEstudos.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20201129011958_SecondMigrations")]
+    partial class SecondMigrations
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ BlogEstudos/Migrations/20201129011958_SecondMigrations.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BlogEstudos.Migrations
+{
+    public partial class SecondMigrations : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "MateriaId",
+                table: "Debates",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "PublicacoesDebates",
+                columns: table => new
+                {
+                    PublicacaoId = table.Column<int>(nullable: false),
+                    DebateId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PublicacoesDebates", x => new { x.DebateId, x.PublicacaoId });
+                    table.ForeignKey(
+                        name: "FK_PublicacoesDebates_Debates_DebateId",
+                        column: x => x.DebateId,
+                        principalTable: "Debates",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_PublicacoesDebates_Publicacoes_PublicacaoId",
+                        column: x => x.PublicacaoId,
+                        principalTable: "Publicacoes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Debates_MateriaId",
+                table: "Debates",
+                column: "MateriaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PublicacoesDebates_PublicacaoId",
+                table: "PublicacoesDebates",
+                column: "PublicacaoId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Debates_Materias_MateriaId",
+                table: "Debates",
+                column: "MateriaId",
+                principalTable: "Materias",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Debates_Materias_MateriaId",
+                table: "Debates");
+
+            migrationBuilder.DropTable(
+                name: "PublicacoesDebates");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Debates_MateriaId",
+                table: "Debates");
+
+            migrationBuilder.DropColumn(
+                name: "MateriaId",
+                table: "Debates");
+        }
+    }
+}
